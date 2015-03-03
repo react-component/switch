5d4916670cbebaab9d542d76dd3149a209970464
liuqin.sheng <liuqin.sheng@alipay.com>
2015-03-03 14:12:24 +0800

    rc-style support

--- examples/index.js
+++ examples/index.js
@@ -1,8 +1,6 @@
 require('../assets/index.css');
 var React = require('react');
 var Switch = require('rc-switch');
-onChange = function(value){
-    console.log(value);
-}
-React.render(
-    <Switch onChange={onChange}></Switch>, document.getElementById("__react-content"));
+
+var switchui = React.render(
+    <Switch></Switch>, document.getElementById("__react-content"));
