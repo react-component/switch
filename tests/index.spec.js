f4e404c8f1e217b1f08770fe74c40dac23fafbf5
aaa707 <aaa707b@gmail.com>
2017-04-11 17:36:32 +0900

    Update for some deprecation warnings

--- tests/index.spec.js
+++ tests/index.spec.js
@@ -5,7 +5,7 @@ const expect = require('expect.js');
 const Switch = require('../index');
 const React = require('react');
 const ReactDOM = require('react-dom');
-const TestUtils = require('react-addons-test-utils');
+const TestUtils = require('react-dom/test-utils');
 const Simulate = TestUtils.Simulate;
 
 describe('rc-switch', () => {
@@ -26,7 +26,7 @@ describe('rc-switch', () => {
 
   it('works', () =>{
     expect(switcher.state.checked).to.be(false);
-    Simulate.click(React.findDOMNode(switcher));
+    Simulate.click(ReactDOM.findDOMNode(switcher));
     expect(switcher.state.checked).to.be(true);
   });
 });
