ff47a2763fed7d3ff5d5acae5843bfa7ef817d5f
zombiej <smith3816@gmail.com>
2020-05-18 15:18:07 +0800

    update eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,5 +8,7 @@ module.exports = {
     'prefer-promise-reject-errors': 0,
     'react/no-array-index-key': 0,
     'react/sort-comp': 0,
+    'import/no-named-as-default': 0,
+    'import/no-named-as-default-member': 0,
   },
 };
