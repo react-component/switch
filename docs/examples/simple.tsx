f28421d7e700b9de9f2c482ab0f5ac57997816ac
MadCcc <1075746765@qq.com>
2022-09-20 12:10:37 +0800

    chore: migrate to dumi

---
+++ docs/examples/simple.tsx
@@ -0,0 +1,32 @@
+import '../../assets/index.less';
+import React, { useState } from 'react';
+import Switch, { SwitchChangeEventHandler } from 'rc-switch';
+
+const onChange: SwitchChangeEventHandler = (value, event) => {
+  // eslint-disable-next-line no-console
+  console.log(`switch checked: ${value}`, event);
+}
+
+export default () => {
+  const [disabled, setDisabled] = useState(false);
+
+  const toggle = () => {
+    setDisabled((prev) => !prev);
+  }
+
+  return (
+    <div style={{ margin: 20 }}>
+      <Switch
+        onChange={onChange}
+        disabled={disabled}
+        checkedChildren="开"
+        unCheckedChildren="关"
+      />
+      <div style={{ marginTop: 20 }}>
+        <button type="button" onClick={toggle}>
+          toggle disabled
+        </button>
+      </div>
+    </div>
+  )
+}
